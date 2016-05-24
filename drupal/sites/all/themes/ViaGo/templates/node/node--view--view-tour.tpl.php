1ace615cf5801df7d27092558877008a0e23e2cd
IGenerate <brent.stappaerts@gmail.com>
2016-05-24 20:37:47 +0200

    Edited node--view--view--tour.tpl.php, opmaak css

--- drupal/sites/all/themes/ViaGo/templates/node/node--view--view-tour.tpl.php
+++ drupal/sites/all/themes/ViaGo/templates/node/node--view--view-tour.tpl.php
@@ -6,20 +6,17 @@ $img_url = $base_url . '/sites/all/themes/ViaGo/img';
 
 ?>
 
- <section class="for-full-back color-white" id="rondleidingen">
-        <div class="container">
-            <div class="row text-center g-pad-bottom">
-            </div>
-                    <div class="col-md-4 col-sm-2 col-xs-8">
-                        <?php print drupal_render($content['field_tour_coverphoto']); ?>
-                            <div id="rondleiding">
-                                    <div id ="overzicht">
-                                        <h5><?php print $title;?></h5>
-                                        <p><?php print drupal_render($content['field_tour_location']); ?></p>
-                                        <p><?php print drupal_render($content['field_tour_languages']); ?></p>
-                                        <p><?php print drupal_render($content['field_tour_pricecategory']); ?></p>  
-                                    </div>
-                                <a href="<?php print $base_url . '/' . drupal_get_path_alias('node/' . $node->nid);?>" id="leesmeer">Bekijk deze tour ></a>
+            <div id="tour">
+                <?php print drupal_render($content['field_tour_coverphoto']); ?>
+                        <div id="rondleidingTeaser">
+                            <div id="overzicht">
+                                <h5><?php print $title;?></h5>
+                                <p><?php print drupal_render($content['field_tour_location']); ?></p>
+                                <p><?php print drupal_render($content['field_tour_languages']); ?></p>
+                                <p><?php print drupal_render($content['field_tour_pricecategory']); ?></p>  
                             </div>
-                    </div>
+                                <a href="<?php print $base_url . '/' . drupal_get_path_alias('node/' . $node->nid);?>" id="leesmeer">Bekijk deze tour ></a>
+                        </div>
+            </div>
+
 
