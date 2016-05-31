221e097906b8e42edf60c92ead5d1abdfc63456c
IGenerate <brent.stappaerts@gmail.com>
2016-05-31 14:16:42 +0200

    Edited user--login.tpl.php, wachtwoord vergeten link.

--- drupal/sites/all/themes/ViaGo/templates/user-login.tpl.php
+++ drupal/sites/all/themes/ViaGo/templates/user-login.tpl.php
@@ -17,7 +17,7 @@ $image = $base_url . '/sites/all/themes/ViaGo/img';
     					 print drupal_render($form['form_build_id']);
     					 print drupal_render($form['form_id']);
     					 print drupal_render($form['actions']); ?>        
-
+        <a href="<?php print $base_url . '/' . drupal_get_path_alias('user/password');?>">wachtwoord vegeten?</a>
         </div>
         <div class="col-xs-6"><img src="<?php print $image . '/gent_4.jpg'?>" width="100%" /></div>
     </div>
