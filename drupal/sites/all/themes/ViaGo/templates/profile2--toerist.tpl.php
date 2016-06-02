e9859dc04a6ab793f4f2fe2e823fdf480a153651
IGenerate <brent.stappaerts@gmail.com>
2016-06-02 18:23:30 +0200

    Add profile2--guide.tpl.php en profile--toerist.tpl.php

--- drupal/sites/all/themes/ViaGo/templates/profile2--toerist.tpl.php
+++ drupal/sites/all/themes/ViaGo/templates/profile2--toerist.tpl.php
@@ -1,9 +1,7 @@
-
-
-
 <?php
 
 global $base_url;
+global $user;
 $account = user_load($user->uid);
 
 
@@ -15,14 +13,11 @@ $account = user_load($user->uid);
              <div class="col-xs-6" id="accountform">
                 <div class="row">
                     <div class="col-xs-6">
-                        <?php print drupal_render($content['field_user_guide_profilepic']); ?>
+
                     </div>
                     <div class="col-xs-6">
                         <ul id="gegevens">
                             <strong><?php print $account->field_user_firstname[LANGUAGE_NONE][0]['value']; ?> <?php print $account->field_user_name[LANGUAGE_NONE][0]['value']; ?> </strong>
-                            <li><?php print render($content['field_user_guide_birthdate']); ?></li>
-                            <li><?php print render($content['field_user_guide_experience']); ?></li>
-                            <li><?php print render($content['field_user_guide_languages']); ?></li>
                         </ul>
                         
                         
@@ -36,20 +31,15 @@ $account = user_load($user->uid);
 
         </div>
                     <div id="acountbeschrijving">
-                        <h2>Over <?php print $account->field_user_firstname[LANGUAGE_NONE][0]['value']; ?> </h2>
-                        <p><?php print render($content['field_user_guide_description']); ?></p>
+                        <h2>Accountgegevens </h2> 
+                        <p>Username: <?php  print $user->name; ?> </p>
+                        <p>E-mailadres: <?php print $user->mail; ?></p>
+                        <p>Wachtwoord: ****** </p>
+
+                       
+
                         
-                        <h2>Waarom <?php print $account->field_user_firstname[LANGUAGE_NONE][0]['value']; ?> Boeken?</h2>
-                    </div>
+                        <h2>Mijn favoriete rondleidingen</h2>
+                   </div>
     </div>
 </body>
-
-
-
-
-
-
-
-
-
-
