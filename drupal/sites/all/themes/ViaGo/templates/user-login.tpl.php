c13a3e826c178301a5fbadd8c962c6add00f8ad0
IGenerate <brent.stappaerts@gmail.com>
2016-06-07 18:51:08 +0200

    Edited user-login.tpl.php

--- drupal/sites/all/themes/ViaGo/templates/user-login.tpl.php
+++ drupal/sites/all/themes/ViaGo/templates/user-login.tpl.php
@@ -11,7 +11,7 @@ $image = $base_url . '/sites/all/themes/ViaGo/assets/img';
 <body id="inloggen">
 <div id="login">  
     <div class="row">
-        <div class="col-xs-6" id="loginform">
+        <div class="col-xs-6" id="loginformlogin">
         <h2>Meld je hier aan</h2>
                    <?php print drupal_render_children($form) ?>       
         <a href="<?php print $base_url . '/' . drupal_get_path_alias('user/password');?>">Wachtwoord vegeten?</a>
