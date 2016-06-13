bc6335c769a7fbc2bfdf48e2d645dd4a28f9cc76
IGenerate <brent.stappaerts@gmail.com>
2016-06-13 11:01:03 +0200

    Edited, profile2--toerist.tpl.php

--- drupal/sites/all/themes/ViaGo/templates/profile2--toerist.tpl.php
+++ drupal/sites/all/themes/ViaGo/templates/profile2--toerist.tpl.php
@@ -13,28 +13,37 @@ $account = user_load($user->uid);
              <div class="col-xs-6" id="accountform">
                 <div class="row">
                     <div class="col-xs-6">
-                        
                     </div>
                     <div class="col-xs-6">
-                        <ul id="gegevens">
-                            
-                        </ul>
+                        <div id="NaamToerist">
+                        <h1> <?php  print $user->name; ?> </h1>
+
+                        </div>                    
                         
-                        
-                    </div>
                 </div>
+            </div>
+                <div class="row">
+                    <div class="col-xs-6">
+                    </div>
+                    <div class="col-xs-6">
+                        <div id="gegevens">
+                        <h2>Accountgegevens </h2> 
+                        <p><strong>Profilenaam:</strong> <?php  print $user->name; ?> </p>
+                        <p><strong>E-mailadres:</strong> <?php print $user->mail; ?></p>
+                        <p><strong>Wachtwoord:</strong> ****** </p>
+                        </div>
+                        <div id="favorieten">
+                        <h2>Mijn favoriete rondleidingen </h2> 
+                        </div>
+                        
+            </div>
+                        
+
 
             </div>
-            <div class="col-xs-6">
-               
-            </div>
+
 
         </div>
-                    <div id="acountbeschrijving">
-                        <h2>Accountgegevens </h2> 
-                        <p>Username: <?php  print $user->name; ?> </p>
-                        <p>E-mailadres: <?php print $user->mail; ?></p>
-                        <p>Wachtwoord: ****** </p>
-                   </div>
+
     </div>
 </body>
