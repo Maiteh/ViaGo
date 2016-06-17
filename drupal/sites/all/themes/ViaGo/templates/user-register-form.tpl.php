00ddcfad590f05b101263dc8eec0e4f41a2d1085
IGenerate <brent.stappaerts@gmail.com>
2016-06-17 17:23:17 +0200

    Edited CSS, _user_register.scss, inlog users aangepast

--- drupal/sites/all/themes/ViaGo/templates/user-register-form.tpl.php
+++ drupal/sites/all/themes/ViaGo/templates/user-register-form.tpl.php
@@ -6,9 +6,9 @@ $image = $base_url . '/sites/all/themes/ViaGo/assets/img';
 
 ?>
 
-<body id="inloggen">
-<div id="login">  
-        <div id="loginform">
+<body id="registreren">
+<div id="register">  
+        <div id="registerform">
         <h2>Registreren</h2>
            <?php print drupal_render_children($form) ?>       
     </div>
